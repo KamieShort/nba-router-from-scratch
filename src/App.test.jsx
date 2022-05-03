d9074d133550fb7561ef3033dfb9549091a5768b
Kamie Short <kamie.short@gmail.com>
2022-05-03 16:13:04 -0700

    adjusted initialEntries path to test details page directly

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
 describe('App', () => {
   it('Should render the list of characters', async () => {
     render(
-      <MemoryRouter initialEntries={['/characters']}>
+      <MemoryRouter initialEntries={['/characters/:id']}>
         <App />
       </MemoryRouter>
     );
 
-   
+    const heading = await screen.findByText('Rick and Morty!');
+    expect(heading).toBeInTheDocument();
   });
 });
